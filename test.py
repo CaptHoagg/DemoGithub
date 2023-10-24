25a5634df3dcad5e25e2820e9ea6c624c7fa27db
CaptHoagg <anhhoang.021203@gmail.com>
2023-10-24 21:24:34 +0700

    HIhi i finally did it

---
+++ test.py
@@ -0,0 +1,52 @@
+# một cái app có thể đọc ghi file
+# có 3 options:
+
+# xem playlist
+# file txt lưu tất cả các bài hát đã được người dùng nhập vào từ trước
+# thêm bài hát vào playlist
+# thêm bài này vào file txt luôn
+# chạy một bài trong playlist và nhấn play
+# call được browser để chạy
+
+def display_mainmenu():
+    print("---------")
+    print("---------")
+    print("---------")
+    print("---------")
+    # this fuction is created to display the menu of our console app
+    pass
+
+
+def add_song_to_playlist():
+    # require user to enter the name of the song
+    # require user to enter the url of the song
+    # save the data to txt file
+    pass
+
+
+def show_playlist():
+    # read the txt file
+    # print out all name of song in the playlist
+    pass
+
+
+def play_song():
+    # require user to enter the id of the song
+    # play that song
+    pass
+
+
+def user_option():
+    # require user to exactly enter the right option (1-2-3)
+    # if 1 -> show playlist
+    # if 2 -> add song to playlist
+    # if 3 -> play a song in playlist
+    pass
+
+
+def main():
+    # this is the main function of the app, use this to handle all your application
+    pass
+
+
+main()
