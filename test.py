fb0f5188c849c819c51126c67670de4d45e27461
nhynhi23411 <nhinhy23411@st.uel.edu.vn>
2023-10-27 16:49:18 +0700

    nhynhi kiki

--- test.py
+++ test.py
@@ -9,17 +9,22 @@
 # call được browser để chạy
 
 def display_mainmenu():
-    print("---------")
-    print("---------")
-    print("---------")
-    print("---------")
-    print("nhynhi ")
-    print("a b")
+    print("Welcome to Nhynhi App")
+    print("1. Add song to playlist")
+    print("2. Show playlist")
+    print("3. Play song")
     # this fuction is created to display the menu of our console app
     pass
 
 
 def add_song_to_playlist():
+    song_name = input("Please enter the name of the song:")
+    song_url = input("Please enter the url of the song:")
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"Song Name: {song_name}\n")
+        data_file.write(f"Song URL: {song_url}\n")
+
+    print(f"{song_name} has been added to the playlist.")
     # require user to enter the name of the song
     # require user to enter the url of the song
     # save the data to txt file
@@ -29,6 +34,24 @@ def add_song_to_playlist():
 def show_playlist():
     # read the txt file
     # print out all name of song in the playlist
+
+    try:
+        with open("data.txt", "r") as data_file:
+            song = data_file.read()
+        
+        song_2 = song.split("\n\n")
+        
+        if song_2:
+            print("Songs in the Playlist:")
+            for entry in song_2:
+                lines = entry.strip().split("\n")
+                for line in lines:
+                    if line.startswith("Song Name:"):
+                        song_name = line[len("Song Name: "):]
+                        print(song_name)
+    except FileNotFoundError:
+        print("The playlist is empty. Please add songs first.")
+
     pass
 
 
