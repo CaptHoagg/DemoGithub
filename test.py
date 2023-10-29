f1445fda83b23cc1b1d403ea3c327e8f8305f5f2
nhynhi23411 <nhinhy23411@st.uel.edu.vn>
2023-10-29 11:01:21 +0700

    nhynhi

--- test.py
+++ test.py
@@ -1,77 +1,67 @@
-# một cái app có thể đọc ghi file
-# có 3 options:
 
-# xem playlist
-# file txt lưu tất cả các bài hát đã được người dùng nhập vào từ trước
-# thêm bài hát vào playlist
-# thêm bài này vào file txt luôn
-# chạy một bài trong playlist và nhấn play
-# call được browser để chạy
-
-def display_mainmenu():
-    print("Welcome to Nhynhi App")
+def main_menu():
     print("1. Add song to playlist")
     print("2. Show playlist")
     print("3. Play song")
-    # this fuction is created to display the menu of our console app
-    pass
+    print("4. Exit")  # Added an option to exit
+    # This function is created to display the menu of our console app
 
 
+playlist = []
 def add_song_to_playlist():
-    song_name = input("Please enter the name of the song:")
-    song_url = input("Please enter the url of the song:")
+    song_name = input("Hãy nhập tên bài hát:")
+    song_url = input("Hãy nhập URL của bài hát:")
+    playlist.append(song_url)
     with open("data.txt", "a") as data_file:
         data_file.write(f"Song Name: {song_name}\n")
         data_file.write(f"Song URL: {song_url}\n")
-
-    print(f"{song_name} has been added to the playlist.")
-    # require user to enter the name of the song
-    # require user to enter the url of the song
-    # save the data to txt file
-    pass
-
+    
+    print(f"{song_name} đã được thêm vào.")
+    # Require the user to enter the name of the song and URL
+    # Save the data to the text file
+    # Add the song url to array 
 
 def show_playlist():
-    # read the txt file
-    # print out all name of song in the playlist
-
     try:
         with open("data.txt", "r") as data_file:
-            song = data_file.read()
-        
-        song_2 = song.split("\n\n")
-        
-        if song_2:
+            song_data = data_file.read()
+
+        song_list = song_data.split("\n\n")
+
+        if song_list:
             print("Songs in the Playlist:")
-            for entry in song_2:
-                lines = entry.strip().split("\n")
-                for line in lines:
-                    if line.startswith("Song Name:"):
-                        song_name = line[len("Song Name: "):]
-                        print(song_name)
+            for entry in song_list:
+                print(entry)
+        else:
+            print("The playlist is empty. Please add songs first.")
     except FileNotFoundError:
         print("The playlist is empty. Please add songs first.")
 
-    pass
-
 
 def play_song():
-    # require user to enter the id of the song
-    # play that song
-    pass
-
+    song_id = input("Hãy chọn nhạc bạn muốn phát:")
+    # Play the selected song (you can implement this functionality using a music player library)
 
 def user_option():
-    # require user to exactly enter the right option (1-2-3)
-    # if 1 -> show playlist
-    # if 2 -> add song to playlist
-    # if 3 -> play a song in playlist
-    pass
-
+    while True:
+        main_menu()
+        chon = input("Hãy lựa chọn 1->4: ")
+        if chon == '1':
+            add_song_to_playlist()
+        elif chon == '2':
+            show_playlist()
+        elif chon == '3':
+            play_song()
+        elif chon == '4':
+            print("Tạm biệt!")
+            break
+        else:
+            print("Lựa chọn không có, hãy chọn lựa chọn khác")
 
 def main():
-    # this is the main function of the app, use this to handle all your application
-    pass
+    print("Chào mừng bạn đã đến với app của Nhynhi:")
+    user_option()
 
 
-main()
+if __name__ == "__main__":
+    main()
